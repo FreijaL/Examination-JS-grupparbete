b2bb8f0b5ebf8db6beda96a2aec737ca25375050
KamalaLundin <107465342+KamalaLundin@users.noreply.github.com>
2023-01-25 10:15:15 +0100

    mini uppdatering

--- script.js
+++ script.js
@@ -15,10 +15,10 @@
 
 
 
-// 1 - lista med alfabetet (ta bort för varje gissningar)
-//   - lista med rätt gissade bokstäver
+// 1 - lista med alfabetet - "lettersOptions" (ta bort för varje gissningar?)
+//   - lista med rätt gissade bokstäver - "correctLetters"
 //   - lista med fel gissade bokstäver
-//   - lista med random ord
+//   - lista med random ord - "words"
 //   - max antal gissningar
 //   - antal gissningar användaren gjort
 // 2 - vid varje tangenttryck ska en function köras som jämför tryck med if (bokstav)= true else(bokstav) = false
@@ -61,7 +61,7 @@ let imgLegs = document.querySelector('#legs');
 
 let imgOpacity = [imgGround, imgScaffold, imgHead, imgBody, imgArms, imgLegs];
 
-let lettersOptions = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","å","ä","ö"];
+let lettersOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö"];
 let words = ['adam', 'kamala', 'freija'];
 let currentWord = [];
 let correctLetters = '';
@@ -77,7 +77,7 @@ let wrongLetterGuess = [];
 
 
 function startGame() {
-   
+
     let randomWord = Math.floor(Math.random() * words.length);
     currentWord.push(words[randomWord]);
 
@@ -87,8 +87,8 @@ function startGame() {
 
     // när vi fått ett random ord ska det skapas li i ul('word')
     // = antal bokstäver i ordet
-    
-    for (let i = 0; i < correctLetters.length; i++){
+
+    for (let i = 0; i < correctLetters.length; i++) {
         var wordListEl = document.createElement('li');
         wordListEl.innerHTML = '';
         correctWord.appendChild(wordListEl);
@@ -104,32 +104,32 @@ function startGame() {
     let keyBoard = document.querySelector('body')
     keyBoard.addEventListener('keypress', fetchkey => {
         var fetchKey = fetchkey.key;
-    //- vid tryck - jämför bokstav(tryck) med nuvarande ord "currentWord"
-    
+        //- vid tryck - jämför bokstav(tryck) med nuvarande ord "currentWord"
 
-    for (let index = 0; index < correctLetters.length; index++) {
-        //Fråga till MAJA: Hur får vi rätt index att hamna på rätt <li>?
-        if (correctLetters.includes(fetchkey.key) === true) {
-            console.log('WORK');
-            wordListEl.innerHTML = fetchkey.key; 
-        } else {
-        //FRÅGA TILL MAJA: Hur får vi alla index att hamna i "nomatch"?
-            console.log('NOT WORK');
-            wrongGuess.textContent = fetchkey.key; 
-            console.log(wrongLetterGuess);
 
-        //FRÅGA TILL MAJA: Hur får vi en bild att visas per felaktigt tryck. 
-            for (let img = 0; img < imgOpacity.length; img++) {
-                imgOpacity[0].style.opacity = 1;
+        for (let index = 0; index < correctLetters.length; index++) {
+            //FRÅGA TILL MAJA: Hur får vi rätt index att hamna på rätt <li>?
+            if (correctLetters.includes(fetchkey.key) === true) {
+                console.log('WORK');
+                wordListEl.innerHTML = fetchkey.key;
+            } else {
+                //FRÅGA TILL MAJA: Hur får vi alla index att hamna i "nomatch"?
+                console.log('NOT WORK');
+                wrongGuess.textContent = fetchkey.key;
+                console.log(wrongLetterGuess);
+
+                //FRÅGA TILL MAJA: Hur får vi en bild att visas per felaktigt tryck. 
+                for (let img = 0; img < imgOpacity.length; img++) {
+                    imgOpacity[0].style.opacity = 1;
+                }
+
             }
-          
-        }
 
-    }
+        }
 
     });
 
- //Går inte att placera bokstäverna på rätt plats i ordet. Allt hamnar på sista rutan. 
+    //Går inte att placera bokstäverna på rätt plats i ordet. Allt hamnar på sista rutan. 
     //console.log(correctLetters.indexOf('m'));
 
 }
